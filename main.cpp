6de29224acda29585f3f1ee42e21d8e8bade9a6d
rgbitx <rgbitx@gmail.com>
2016-04-11 01:32:42 +0900

    t1

--- main.cpp
+++ main.cpp
@@ -3,35 +3,52 @@
 
 using namespace std;
 
-bool searchMatrix(vector<vector<int>> &matrix, int target){
+bool searchMatrix(vector<vector<int>> &matrix, int target) {
 
-    int rowBegin = 0, rowEnd = matrix.size()-1;
-    int colBegin = 0, colEnd = matrix[0].size()-1;
-    int rowMid = 0, colMid = 0;
+    if (matrix.empty()) {
+        return false;
+    }
 
-    while (rowBegin + 1 < rowEnd){
-        rowMid = rowBegin + (rowEnd - rowBegin)/2;
+    int rowBegin = 0, rowEnd = matrix.size() - 1;
+    int colBegin = 0, colEnd = matrix[0].size() - 1;
+    int rowMid = 0, colMid = 0, row = 0, col = 0;
 
-        if (matrix[rowMid][0] == target){
+    while (rowBegin + 1 < rowEnd) {
+        rowMid = rowBegin + (rowEnd - rowBegin) / 2;
+
+        if (matrix[rowMid][0] == target) {
             return true;
-        } else if(matrix[rowMid][0] < target){
+        } else if (matrix[rowMid][0] < target) {
             rowBegin = rowMid;
         } else {
             rowEnd = rowMid;
         }
-
     }
 
-    while(colBegin + 1 < colEnd){
-        colMid = colBegin + (colEnd - colBegin)/2;
+    if (matrix[rowEnd][0] <= target) {
+        row = rowEnd;
+    } else if (matrix[rowBegin][0] <= target){
+        row = rowBegin;
+    } else {
+        return  false;
+    }
 
-        if(matrix[rowMid][colMid] == target){
+    while (colBegin + 1 < colEnd) {
+        colMid = colBegin + (colEnd - colBegin) / 2;
+
+        if (matrix[row][colMid] == target) {
             return true;
-        } else if (matrix[rowMid][colMid] < target){
+        } else if (matrix[row][colMid] < target) {
             colBegin = colMid;
         } else {
             colEnd = colMid;
         }
+    }
+
+    if (matrix[row][colBegin] == target){
+        return true;
+    } else if (matrix[row][colEnd] == target){
+        return true;
     }
 
     return false;
@@ -41,20 +58,20 @@ bool searchMatrix(vector<vector<int>> &matrix, int target){
 int main() {
     cout << "Hello, World!" << endl;
 
-    vector<vector<int>> matrix={
-            {1,3,5,7},
-            {10,11,16,20},
-            {23,30,34,50}
+    vector<vector<int>> matrix = {
+            {1,  3,  5,  7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 50}
     };
 
-    int target=19;
+    int target = 50;
 
-    bool ret=searchMatrix(matrix, target);
+    bool ret = searchMatrix(matrix, target);
 
     if (ret) {
-        cout<<"Yes";
+        cout << "Yes";
     } else {
-        cout<<"No";
+        cout << "No";
     }
 
     return 0;
